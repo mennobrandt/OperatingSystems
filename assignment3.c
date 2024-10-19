77642e8c5182e7763efa2000b7f28ae65929da99
katte-kwaad <mennobrandt@protonmail.com>
2024-10-19 21:15:41 +1030

    simplification of method and var names

--- assignment3.c
+++ assignment3.c
@@ -19,62 +19,58 @@
 
     typedef struct node {
         char *line;
-        int contains_pattern;
-        struct node *next; // next in the shared list
-        struct node *book_next; // next in the same book
-        struct node *next_frequent_search; // nodes containing the search pattern
+        int has_ptrn; // has pattern
+        struct node *next; 
+        struct node *next_book; 
+        struct node *next_freq_search; // next frequent 
     } node_t;
 
     typedef struct book {
-        int connection_order;
+        int con_order; //connection order
         char *title;
         node_t *head;
         node_t *tail;
         int search_count;
-        struct book *next; // For keeping track of all books
+        struct book *next; 
     } book_t;
 
     typedef struct {
         int socket_fd;
-        int connection_order;
+        int con_order;
     } client_info_t;
 
-    // Global variables
-    node_t *shared_head = NULL;
-    node_t *shared_tail = NULL;
+    node_t *same_head = NULL;
+    node_t *same_tail = NULL;
     pthread_mutex_t shared_list_mutex = PTHREAD_MUTEX_INITIALIZER;
 
     book_t *books_list = NULL; // Head of the books list
     pthread_mutex_t books_list_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-    char *search_pattern = NULL;
+    char *search_ptrn = NULL;
 
-    // Function declarations
     void *client_handler(void *arg);
     void *analysis_thread_func(void *arg);
-    int compare_books(const void *a, const void *b);
+    int comp_books(const void *a, const void *b);
 
     int main(int argc, char *argv[]) {
         int listen_port = 0;
         int opt;
-
-        // Parse command line arguments
         while ((opt = getopt(argc, argv, "l:p:")) != -1) {
             switch (opt) {
                 case 'l':
                     listen_port = atoi(optarg);
                     break;
                 case 'p':
-                    search_pattern = optarg;
+                    search_ptrn = optarg;
                     break;
                 default:
-                    fprintf(stderr, "Usage: %s -l listen_port -p search_pattern\n", argv[0]);
+                    fprintf(stderr, "Usage: %s -l listen_port -p search_ptrn\n", argv[0]);
                     exit(EXIT_FAILURE);
             }
         }
 
-        if (listen_port == 0 || search_pattern == NULL) {
-            fprintf(stderr, "Usage: %s -l listen_port -p search_pattern\n", argv[0]);
+        if (listen_port == 0 || search_ptrn == NULL) {
+            fprintf(stderr, "Usage: %s -l listen_port -p search_ptrn\n", argv[0]);
             exit(EXIT_FAILURE);
         }
 
@@ -82,20 +78,19 @@
         struct sockaddr_in address;
         int addrlen = sizeof(address);
 
-        // Create server socket
+        // make the server socket
         if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
             perror("socket failed");
             exit(EXIT_FAILURE);
         }
 
-        // Set socket options (reuse address)
         int optval = 1;
         if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval))) {
             perror("setsockopt");
             exit(EXIT_FAILURE);
         }
 
-        // Bind to the specified port
+        // bind to the arg port
         address.sin_family = AF_INET;
         address.sin_addr.s_addr = INADDR_ANY; // accept any incoming interface
         address.sin_port = htons(listen_port);
@@ -105,7 +100,7 @@
             exit(EXIT_FAILURE);
         }
 
-        // Start listening
+        //start listening
         if (listen(server_fd, 10) < 0) {
             perror("listen");
             exit(EXIT_FAILURE);
@@ -113,7 +108,6 @@
 
         printf("Server listening on port %d\n", listen_port);
 
-        // Create analysis threads
         pthread_t analysis_threads[ANALYSIS_THREAD_COUNT];
         for (int i = 0; i < ANALYSIS_THREAD_COUNT; i++) {
             if (pthread_create(&analysis_threads[i], NULL, analysis_thread_func, NULL) != 0) {
@@ -124,7 +118,6 @@
 
         int connection_counter = 0;
 
-        // Accept connections
         while (1) {
             int new_socket;
             struct sockaddr_in client_address;
@@ -139,9 +132,9 @@
 
             client_info_t *client_info = malloc(sizeof(client_info_t));
             client_info->socket_fd = new_socket;
-            client_info->connection_order = connection_counter;
+            client_info->con_order = connection_counter;
 
-            // Create a new thread to handle the client
+            // new thread
             pthread_t thread_id;
             if (pthread_create(&thread_id, NULL, client_handler, (void *)client_info) != 0) {
                 perror("pthread_create");
@@ -150,11 +143,9 @@
                 continue;
             }
 
-            // Detach the thread so that resources are freed when it exits
             pthread_detach(thread_id);
         }
 
-        // Close the server socket (unreachable code in this case)
         close(server_fd);
         return 0;
     }
@@ -162,10 +153,10 @@
     void *client_handler(void *arg) {
         client_info_t *client_info = (client_info_t *)arg;
         int socket_fd = client_info->socket_fd;
-        int connection_order = client_info->connection_order;
+        int con_order = client_info->con_order;
         free(client_info);
 
-        // Set socket to non-blocking mode
+        // socket's gotta be non blocking
         int flags = fcntl(socket_fd, F_GETFL, 0);
         fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK);
 
@@ -182,7 +173,7 @@
         char *book_title = NULL;
         int first_line = 1;
 
-        // Read data from the socket
+        // read from da socket
         while (1) {
             bytes_read = recv(socket_fd, recv_buffer, BUFFER_SIZE - 1, 0);
             if (bytes_read > 0) {
@@ -190,42 +181,37 @@
                 for (i = 0; i < bytes_read; i++) {
                     char c = recv_buffer[i];
                     if (c == '\n') {
-                        // End of line
                         line_buffer[line_buffer_len] = '\0';
-                        // Remove carriage return if present
                         if (line_buffer_len > 0 && line_buffer[line_buffer_len - 1] == '\r') {
                             line_buffer[line_buffer_len - 1] = '\0';
                         }
 
-                        // Create a new node
+                        // new node
                         node_t *new_node = malloc(sizeof(node_t));
                         new_node->line = strdup(line_buffer);
-                        new_node->contains_pattern = (strstr(line_buffer, search_pattern) != NULL);
+                        new_node->has_ptrn = (strstr(line_buffer, search_ptrn) != NULL);
                         new_node->next = NULL;
-                        new_node->book_next = NULL;
-                        new_node->next_frequent_search = NULL;
+                        new_node->next_book = NULL;
+                        new_node->next_freq_search = NULL;
 
-                        // Lock the shared list mutex
                         pthread_mutex_lock(&shared_list_mutex);
 
-                        // Add to the shared list
-                        if (shared_tail == NULL) {
-                            shared_head = shared_tail = new_node;
+                        if (same_tail == NULL) {
+                            same_head = same_tail = new_node;
                         } else {
-                            shared_tail->next = new_node;
-                            shared_tail = new_node;
+                            same_tail->next = new_node;
+                            same_tail = new_node;
                         }
 
                         pthread_mutex_unlock(&shared_list_mutex);
 
-                        // Process book list
                         if (first_line) {
                             book_title = strdup(line_buffer);
                             first_line = 0;
 
-                            // Create a new book entry and add to the books list
+                            // new book entry and add to the list
                             book_t *new_book = malloc(sizeof(book_t));
-                            new_book->connection_order = connection_order;
+                            new_book->con_order = con_order;
                             new_book->title = strdup(book_title);
                             new_book->head = new_node;
                             new_book->tail = new_node;
@@ -233,32 +219,26 @@
                             new_book->next = NULL;
 
                             pthread_mutex_lock(&books_list_mutex);
-                            // Add to the books list
                             new_book->next = books_list;
                             books_list = new_book;
                             pthread_mutex_unlock(&books_list_mutex);
                         } else {
-                            // Add to the book's linked list
                             if (book_tail == NULL) {
                                 book_head = book_tail = new_node;
                             } else {
-                                book_tail->book_next = new_node;
+                                book_tail->next_book = new_node;
                                 book_tail = new_node;
                             }
                         }
 
-                        // Print a line reporting the addition
                         printf("Added node: %s\n", line_buffer);
-
-                        // Reset line buffer
                         line_buffer_len = 0;
                         memset(line_buffer, 0, BUFFER_SIZE);
                     } else {
-                        // Append character to line buffer
                         if (line_buffer_len < BUFFER_SIZE - 1) {
                             line_buffer[line_buffer_len++] = c;
                         } else {
-                            // Line too long, discard or handle error
+                            // line is too long
                             fprintf(stderr, "Line too long, discarding\n");
                             line_buffer_len = 0;
                         }
@@ -267,12 +247,11 @@
 
             } else if (bytes_read == 0) {
                 // Connection closed by client
-                printf("Connection closed by client %d\n", connection_order);
+                printf("Connection closed by client %d\n", con_order);
                 break;
             } else {
                 if (errno == EWOULDBLOCK || errno == EAGAIN) {
-                    // No data available, non-blocking mode
-                    usleep(100000); // Sleep for 100ms
+                    usleep(100000); // 100 ms sleepe
                     continue;
                 } else {
                     perror("recv");
@@ -281,19 +260,17 @@
             }
         }
 
-        // When connection closes, write the received book to a file
         char filename[256];
-        snprintf(filename, sizeof(filename), "book_%02d.txt", connection_order);
+        snprintf(filename, sizeof(filename), "book_%02d.txt", con_order);
 
         FILE *fp = fopen(filename, "w");
         if (fp == NULL) {
             perror("fopen");
         } else {
-            // Traverse the book's linked list and write to file
             pthread_mutex_lock(&books_list_mutex);
             book_t *book = books_list;
             while (book != NULL) {
-                if (book->connection_order == connection_order) {
+                if (book->con_order == con_order) {
                     break;
                 }
                 book = book->next;
@@ -304,7 +281,7 @@
                 node_t *current = book->head;
                 while (current != NULL) {
                     fprintf(fp, "%s\n", current->line);
-                    current = current->book_next;
+                    current = current->next_book;
                 }
             }
             fclose(fp);
@@ -315,33 +292,29 @@
     }
 
     void *analysis_thread_func(void *arg) {
-        // Periodically analyze the shared data
         while (1) {
-            sleep(5); // Configurable interval
+            sleep(5); ///////// can change.........
 
-            // Lock to ensure only one thread outputs at a time
+            // Lock!!!
             static pthread_mutex_t output_mutex = PTHREAD_MUTEX_INITIALIZER;
             if (pthread_mutex_trylock(&output_mutex) == 0) {
-                // We got the lock, proceed to analyze and output
-
-                // Analyze books
                 pthread_mutex_lock(&books_list_mutex);
                 book_t *book = books_list;
                 while (book != NULL) {
                     int count = 0;
                     node_t *node = book->head;
                     while (node != NULL) {
-                        if (node->contains_pattern) {
+                        if (node->has_ptrn) {
                             count++;
                         }
-                        node = node->book_next;
+                        node = node->next_book;
                     }
                     book->search_count = count;
                     book = book->next;
                 }
                 pthread_mutex_unlock(&books_list_mutex);
 
-                // Count books
+                // count books
                 int book_count = 0;
                 pthread_mutex_lock(&books_list_mutex);
                 book = books_list;
@@ -350,7 +323,6 @@
                     book = book->next;
                 }
 
-                // Copy books to array
                 book_t **book_array = malloc(book_count * sizeof(book_t *));
                 int index = 0;
                 book = books_list;
@@ -360,32 +332,27 @@
                 }
                 pthread_mutex_unlock(&books_list_mutex);
 
-                // Sort the array
-                qsort(book_array, book_count, sizeof(book_t *), compare_books);
+                // sort array
+                qsort(book_array, book_count, sizeof(book_t *), comp_books);
 
-                // Output the sorted list
-                printf("\nBooks sorted by occurrences of '%s':\n", search_pattern);
+                // print sorted books
+                printf("\nBooks sorted by occurrences of '%s':\n", search_ptrn);
                 for (int i = 0; i < book_count; i++) {
-                    printf("Book %02d: '%s' - %d occurrences\n", book_array[i]->connection_order, book_array[i]->title, book_array[i]->search_count);
+                    printf("Book %02d: '%s' - %d occurrences\n", book_array[i]->con_order, book_array[i]->title, book_array[i]->search_count);
                 }
                 printf("\n");
-
                 free(book_array);
-
                 pthread_mutex_unlock(&output_mutex);
             } else {
-                // Another analysis thread is outputting, skip this interval
                 continue;
             }
         }
         return NULL;
     }
 
-    // Comparison function for qsort
-    int compare_books(const void *a, const void *b) {
+    int comp_books(const void *a, const void *b) {
         book_t *book_a = *(book_t **)a;
         book_t *book_b = *(book_t **)b;
 
-        // Sort in descending order of search_count
-        return book_b->search_count - book_a->search_count;
+        return (book_b->search_count - book_a->search_count);
     }
